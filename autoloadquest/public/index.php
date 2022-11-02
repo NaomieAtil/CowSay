b85c4dac7c1112b09e8ed0437051dd86c87a17b7
NaomieAtil <naomieatil@gmail.com>
2022-11-02 11:19:06 +0100

    First commit

---
+++ autoloadquest/public/index.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\public;
+
+class Hey {
+public function __construct()
+{
+    echo "Hello Wilders";
+}
+}
---
+++ autoloadquest/src/Wcs/Hello.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Wcs;
+
+class Hello {
+public function __construct()
+{
+    echo "Hello World";
+}
+}
