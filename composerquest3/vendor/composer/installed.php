921f61bac24fdd58728c1a4c38a8a844bbab11f1
NaomieAtil <naomieatil@gmail.com>
2022-11-02 12:16:40 +0100

    cow talking

---
+++ composerquest3/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b85c4dac7c1112b09e8ed0437051dd86c87a17b7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b85c4dac7c1112b09e8ed0437051dd86c87a17b7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
